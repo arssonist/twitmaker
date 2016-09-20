2f14e83747db10cd5d072d81a5e2c6f9a37183ba
Your Name <arssonist@yahoo.com>
2016-09-20 17:10:09 -0400

    post request working via ajax

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -1,2 +1,15 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(function(){
+  $("#create-tweet").on("click", function(event){
+
+  var url = $(this).attr('href');
+  $.ajax({
+    url:'/tweets', 
+    method:'POST',
+    data:{},
+    dataType:'html',
+  }).done(function(response){
+  });
+  });
+});
