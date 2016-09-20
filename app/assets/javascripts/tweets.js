5bbabb22b95d47bfa26e42d851a9cc884ad3952d
Your Name <arssonist@yahoo.com>
2016-09-20 17:44:41 -0400

    html post function almost working

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -2,14 +2,17 @@
 // All this logic will automatically be available in application.js.
 $(function(){
   $("#create-tweet").on("click", function(event){
-
+    var newTweet = $('#new_tweet').serialize();
+    console.log(newTweet);
+    event.preventDefault();
   var url = $(this).attr('href');
   $.ajax({
-    url:'/tweets', 
+    url:'/tweets',
     method:'POST',
-    data:{},
+    data:{tweet:message},
     dataType:'html',
   }).done(function(response){
+    console.log(response);
   });
-  });
-});
+})
+})
